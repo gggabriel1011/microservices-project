8d042a2ea89888d964daf884515814a2471b5732
Gabriel-10 <gabygarciag10@gmail.com>
2025-03-27 21:18:43 -0400

    new - add delete-profile route

--- ms-crud/src/routes/profile.routes.ts
+++ ms-crud/src/routes/profile.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import createProfile from "../controllers/profile.controller";
 import { updateProfile } from "../controllers/profile.controller";
+import { deleteProfile } from "../controllers/profile.controller";
 
 const router = Router();
 
 router.post("/create-profile", createProfile);
 router.put("/update-profile", updateProfile);
+router.delete("/delete-profile", deleteProfile)
 
 export default router;
