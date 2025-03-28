fd19f7ae764b0a42832c74718df96b99e5c75b09
Gabriel-10 <gabygarciag10@gmail.com>
2025-03-28 19:28:37 -0400

    globally basicAuth configuration on ms-profile service

--- ms-profile/src/index.ts
+++ ms-profile/src/index.ts
@@ -3,18 +3,25 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import profileRoutes from "./routes/profile.routes";
 
+import { verifyBasicAuth } from "./middleware/basicAuth.middleware";
+
+import type { RequestHandler } from "express";
+
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 4001;
 
 app.use(express.json());
 
-// Conectamos las rutas
+app.use(verifyBasicAuth as RequestHandler);
+
 app.use("/api", profileRoutes);
 
-const PORT = process.env.PORT || 4001;
-
-mongoose.connect(process.env.MONGO_URI as string)
-    .then(() => app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    }))
-    .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`MS-PROFILE running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
