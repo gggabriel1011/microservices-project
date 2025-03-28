b51d0c14fadad3a85761b866fe043195d3f7baa2
Gabriel-10 <gabygarciag10@gmail.com>
2025-03-27 20:58:25 -0400

    add update-profile route

--- ms-crud/src/routes/profile.routes.ts
+++ ms-crud/src/routes/profile.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import createProfile from "../controllers/profile.controller";
+import { updateProfile } from "../controllers/profile.controller";
 
 const router = Router();
 
 router.post("/create-profile", createProfile);
+router.put("/update-profile", updateProfile);
 
 export default router;
