45c4f2159e63109df436a8da396fe280b0e55717
Gabriel-10 <gabygarciag10@gmail.com>
2025-03-27 20:51:36 -0400

    Database and index configuration

--- ms-crud/src/index.ts
+++ ms-crud/src/index.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/database";
+import profileRoutes from "./routes/profile.routes";
 
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 4002;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("MS-CRUD running");
-});
+app.use("/api", profileRoutes);
 
-app.listen(PORT, () => {
-  console.log(`MS-CRUD running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 4002;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`MS-CRUD running on http://localhost:${PORT}`);
+  });
 });
