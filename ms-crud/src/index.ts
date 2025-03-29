64c25079dbca772f93617d3d8d10537369d72593
Gabriel-10 <gabygarciag10@gmail.com>
2025-03-28 20:41:32 -0400

    Add HTTPS using SLL certificate

--- ms-crud/src/index.ts
+++ ms-crud/src/index.ts
@@ -5,6 +5,14 @@ import profileRoutes from "./routes/profile.routes";
 
 import { verifyBasicAuth } from "./middleware/basicAuth.middleware";
 
+import https from "https";
+import fs from "fs";
+
+const sslOptions = {
+  key: fs.readFileSync("cert.key"),
+  cert: fs.readFileSync("cert.pem")
+};
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4002;
@@ -17,7 +25,10 @@ app.use("/api", profileRoutes);
 
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`MS-CRUD running on http://localhost:${PORT}`);
+  https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`MS-CRUD running on https://localhost:${PORT}`);
   });
+}).catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
 });
+
